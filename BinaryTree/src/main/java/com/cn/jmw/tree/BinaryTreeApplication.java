80e9d41fb27c7cff84263a742aa0f086c78af7ec
王佳 <792836909@qq.com>
2022-08-19 11:02:01 +0800

    增加二叉树查询模块（后期准备加入红黑树 平衡功能）

---
+++ BinaryTree/src/main/java/com/cn/jmw/tree/BinaryTreeApplication.java
@@ -0,0 +1,16 @@
+package com.cn.jmw.tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王佳
+ */
+@SpringBootApplication
+public class BinaryTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryTreeApplication.class, args);
+    }
+
+}

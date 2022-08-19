edfc83867730d2fce4572de3f2af12015d471dea
王佳 <792836909@qq.com>
2022-08-19 10:00:39 +0800

    增加子模块

---
+++ MSSynchronization/src/test/java/com/cn/jmw/mssynchronization/MsSynchronizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.jmw.mssynchronization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSynchronizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Sentinel/src/test/java/com/cn/jmw/sentinel/SentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.jmw.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4dbb2bb98df9d4d3de34f5c7a48d10718d23bcb3
89929665 <89929665@qq.com>
2022-09-20 23:10:06 +0800

    动态数据库 多数据源 H2 sqlite搭建架子

---
+++ UtilsDemo/DynamicDatabase/src/main/java/com.cn.jmw/DynamicDatabaseApplication.java
@@ -0,0 +1,18 @@
+package com.cn.jmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 一只小小狗
+ * @version 1.0.0
+ * @ClassName DynamicDatabaseApplication.java
+ * @Description TODO
+ * @createTime 2022年09月17日 21:00:00
+ */
+@SpringBootApplication
+public class DynamicDatabaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatabaseApplication.class, args);
+    }
+}
